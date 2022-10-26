be48956b70a68513f54f1105e0bbb55d21d5214c
e.g <e.g>
2022-10-26 11:43:12 +0200

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function userHome()
+    {
+        return view('store.home',["msg"=>"Hello! I am user/store"]);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function editorHome()
+    {
+        return view('headoffice.home',["msg"=>"Hello! I am editor/headoffice"]);
+    }
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function adminHome()
+    {
+        return view('admin.home',["msg"=>"Hello! I am admin"]);
+    }
+}
